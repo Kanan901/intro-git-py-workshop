f3b03727cca5b0b01bdd60b18e811694898b15d3
Kanan901 <kanan901@gmail.com>
2018-03-15 20:05:12 -0400

    I am commited

--- caduceus/snake.py
+++ caduceus/snake.py
@@ -1,5 +1,6 @@
 class Snake:
     def __init__(self, length, weight, common_name, sci_name):
+""" hi mojkmsad"""
         self.length = length
         self.weight = weight
         self.common_name = common_name
